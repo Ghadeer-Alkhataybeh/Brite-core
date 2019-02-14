9a5b4299ced450b7d1b8d278eb6a4ad383a5e9b2
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2019-02-14 02:56:59 +0200

    <feat> add Vuex store state

---
+++ src/store/state.js
@@ -0,0 +1,12 @@
+export default {
+  usersInfo: [],
+  searchTerm: "",
+  addedUserName: null,
+  addedDescription: null,
+  addedAmount: null,
+  addedId: null,
+  editedUserName: null,
+  editedDescription: null,
+  editedAmount: null,
+  id: null
+};
